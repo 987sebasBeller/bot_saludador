942ac6c9b028d450728b94a32a3296e38bad8b05
sebastianBeller <sebastianbeller76@gmail.com>
2023-08-10 22:56:31 -0400

    saludo_validando_edad

--- src/abreviador.js
+++ src/abreviador.js
@@ -1,7 +1,8 @@
-function abreviar(genero){
-    var abreviacion="";
-    if (genero=="M"){
-        abreviacion="Sr";
+function abreviar(edad,genero){
+    var abreviacion="Sr";
+    console.log(edad);
+    if (edad<30 || isNaN(edad)){
+        abreviacion=" ";
     }
     else if(genero=="F"){
         abreviacion="Sra";
--- src/presenter.js
+++ src/presenter.js
@@ -3,6 +3,8 @@ const button = document.querySelector("#btn");
 const div=document.querySelector("#saludo");
 const cbx_gender=document.querySelector("#gender");
 const name_input = document.querySelector("#name_input");
+const age_input= document.querySelector("#age_input");
 button.addEventListener("click", ()=>{
-  div.innerHTML=`<p>HOLA ${abreviar(cbx_gender.value)} ${name_input.value} 😃!!</p>`;
+  age= Number.parseInt(age_input.value);
+  div.innerHTML=`<p>HOLA ${abreviar(age,cbx_gender.value)} ${name_input.value} 😃!!</p>`;
 });
