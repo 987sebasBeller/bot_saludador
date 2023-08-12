35c820d66638bf4f7b9feb69b0af25cfac211129
sebastianBeller <sebastianbeller76@gmail.com>
2023-08-12 11:42:24 -0400

    2arreglo_error_saludo_con_traduccion

--- src/presenter.js
+++ src/presenter.js
@@ -9,9 +9,9 @@ const age_input= document.querySelector("#age_input");
 const cbx_language=document.querySelector("#lenguage");
 button.addEventListener("click", async()=>{
   let fecha=new Date();
-  age= Number.parseInt(age_input.value);
-  hora=Number.parseInt(fecha.getHours());
-  texto=`${saludar(hora)} ${abreviar(age,cbx_gender.value)} ${name_input.value}` ;
+  let age= Number.parseInt(age_input.value);
+  let hora=Number.parseInt(fecha.getHours());
+  let texto=`${saludar(hora)} ${abreviar(age,cbx_gender.value)} ${name_input.value}` ;
   texto=await traducir(texto,'en');
   div.innerHTML=`<p>${texto} 😃!!</p>`;
 });
